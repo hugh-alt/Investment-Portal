f737f6d8c5f383b97e4af1e09f0c03abc397790f
Hugh McCaffery <hugh@reachalts.com.au>
2026-03-16 15:14:18 +1100

    Fix pm_truth_layer_v1_2 migration and add CMA v1.2

--- apps-web/prisma/migrations/20260316035143_pm_truth_layer_v1_2/migration.sql
+++ apps-web/prisma/migrations/20260316035143_pm_truth_layer_v1_2/migration.sql
@@ -1,2 +1,13 @@
--- RenameIndex
-ALTER INDEX "PMFundDistributionAllocation_distributionEventId_clientCommi_ke" RENAME TO "PMFundDistributionAllocation_distributionEventId_clientComm_key";
+-- RenameIndex (conditional; index name differs across environments)
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM pg_class c
+    JOIN pg_namespace n ON n.oid = c.relnamespace
+    WHERE c.relkind = 'i'
+      AND c.relname = 'PMFundDistributionAllocation_distributionEventId_clientCommi_ke'
+  ) THEN
+    EXECUTE 'ALTER INDEX "PMFundDistributionAllocation_distributionEventId_clientCommi_ke" RENAME TO "PMFundDistributionAllocation_distributionEventId_clientComm_key"';
+  END IF;
+END $$;
